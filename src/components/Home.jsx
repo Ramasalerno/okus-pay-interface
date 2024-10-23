9a584e4af16459fdd30da9bb1189e9d306249226
ramiro <ramiro_salerno@solutionbox.com.ar>
2024-10-23 15:02:35 -0300

    efectos gsap agregados

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,13 +1,32 @@
 import { useNavigate } from 'react-router-dom';
+import { useRef } from 'react';
+import { gsap } from 'gsap';
 import '../App.css';
 
 function Home() {
     const navigate = useNavigate();
+    const buttonRef = useRef(null); // Referencia para el botón
+
+    const handleClick = () => {
+        // Efecto de explosión
+        gsap.to(buttonRef.current, {
+            scale: 5.5,
+            opacity: 0.5,
+            duration: 0.8,
+            ease: "power1.out",
+            onComplete: () => {
+                navigate('/carrito');
+            }
+        });
+    };
+
     return (
         <div className="contenedorComenzar">
             <button
                 className="start-button"
-                onClick={() => navigate('/carrito')}>
+                onClick={handleClick}
+                ref={buttonRef} // Asignar la referencia al botón
+            >
                 Comenzar
             </button>
         </div>
